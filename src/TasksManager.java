c9ce77e1ec3a108f8cc35356a3a65ce75f10b04b
imposmas <imposmas@yandex.ru>
2024-11-03 17:42:23 +0300

    Second attempt to pass final task for Sprint4 - deleted semicolons

--- src/TasksManager.java
+++ src/TasksManager.java
@@ -16,7 +16,7 @@ public class TasksManager {
             tasksArray.add(tasks.get(task));
         }
         return tasksArray;
-    };
+    }
 
     public List<SubTask> getSubTasks(){
         ArrayList<SubTask> subTasksArray = new ArrayList<>();
@@ -24,7 +24,7 @@ public class TasksManager {
             subTasksArray.add(subTasks.get(subTask));
         }
         return subTasksArray;
-    };
+    }
 
     public List<Epic> getEpics(){
         ArrayList<Epic> epicArrayList = new ArrayList<>();
@@ -32,7 +32,7 @@ public class TasksManager {
             epicArrayList.add(epics.get(epic));
         }
         return epicArrayList;
-    };
+    }
 
     public List<SubTask> getEpicSubTasks(int epicId){
         ArrayList<SubTask> subTasksArray = new ArrayList<>();
@@ -42,19 +42,19 @@ public class TasksManager {
             }
         }
         return subTasksArray;
-    };
+    }
 
     public Task getTask(int id){
         return tasks.get(id);
-    };
+    }
 
     public SubTask getSubTask(int id){
         return subTasks.get(id);
-    };
+    }
 
     public Epic getEpic(int id){
         return epics.get(id);
-    };
+    }
 
     public int addNewTask(Task task){
         uniqueId++;
@@ -69,32 +69,32 @@ public class TasksManager {
         epic.setId(uniqueId);
         calculateEpicStatus(epic);
         return uniqueId;
-    };
+    }
 
     public Integer addNewSubTask(SubTask subTask){
         uniqueId++;
         subTasks.put(uniqueId, subTask);
         subTask.setId(uniqueId);
         return uniqueId;
-    };
+    }
 
     public void updateTask(Task task) {
         tasks.replace(task.getId(),task);
-    };
+    }
 
     public void updateEpic(Epic epic){
         epics.replace(epic.getId(), epic);
         calculateEpicStatus(epic);
-    };
+    }
 
     public void updateSubTask(SubTask subTask){
         subTasks.replace(subTask.getId(), subTask);
         calculateEpicStatus(getEpic(subTask.getEpicId()));
-    };
+    }
 
     public void deleteTask(int id){
         tasks.remove(id);
-    };
+    }
 
     public void deleteEpic(int id){
         ArrayList<Integer> subTasksIdsForDelete = new ArrayList<>();
@@ -107,26 +107,26 @@ public class TasksManager {
             subTasks.remove(subTaskId);
         }
         epics.remove(id);
-    };
+    }
 
     public void deleteSubTask(int id){
         subTasks.remove(id);
         int epicId = getSubTask(id).getEpicId();
         calculateEpicStatus(getEpic(epicId));
-    };
+    }
 
     public void deleteTasks(){
         tasks.clear();
-    };
+    }
 
     public void deleteSubTasks(){
         subTasks.clear();
-    };
+    }
 
     public void deleteEpics(){
         subTasks.clear();
         epics.clear();
-    };
+    }
 
     public void calculateEpicStatus(Epic epic){
         ArrayList<SubTask> subTasksPerEpic = new ArrayList<>();
